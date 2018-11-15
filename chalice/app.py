46aefeacdbeec222a3dd276a6598415df493717d
danilobrinu <ddbn.c2@gmail.com>
2018-11-15 16:31:01 -0500

    Minor changes

--- chalice/app.py
+++ chalice/app.py
@@ -367,8 +367,8 @@ class Response(object):
                 # json serialization results in a string type, but for binary
                 # content types we need a type bytes().  So we need to special
                 # case this scenario and encode the JSON body to bytes().
-                if isinstance(body, str):
-                    body = body.encode('utf-8')
+                # if not isinstance(body, bytes):
+                body = body if isinstance(body, bytes) else body.encode('utf-8')
             body = self._base64encode(body)
             response_dict['isBase64Encoded'] = True
         response_dict['body'] = body
