f0579d572e360cda78f0fe6a7ab372ced62c3d9c
danilobrinu <ddbn.c2@gmail.com>
2018-11-15 16:34:34 -0500

    Minor changes

--- chalice/app.py
+++ chalice/app.py
@@ -368,7 +368,8 @@ class Response(object):
                 # content types we need a type bytes().  So we need to special
                 # case this scenario and encode the JSON body to bytes().
                 # if not isinstance(body, bytes):
-                body = body if isinstance(body, bytes) else body.encode('utf-8')
+                body = body if isinstance(body, bytes) \
+                            else body.encode('utf-8')
             body = self._base64encode(body)
             response_dict['isBase64Encoded'] = True
         response_dict['body'] = body
